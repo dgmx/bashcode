5d67e87fc157df6d732e45f292f3156cd2ffcc17
Diego J. Gonzalez <diego.gm@gmail.com>
2026-05-26 20:11:10 +0200

    Bash

--- PrintNumbers.java
+++
@@ -1,9 +0,0 @@
-public class PrintNumbers {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i <= 100) {
-            System.out.println(i);
-            i++;
-        }
-    }
-}
